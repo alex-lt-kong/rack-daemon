8705b28dd3c588093f0e23b361e208e68ab6a533
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-03-31 17:37:33 +0800

    update signal handler etf

--- src/back/rd.c
+++ src/back/rd.c
@@ -25,10 +25,11 @@ volatile sig_atomic_t done = 0;
 char db_path[PATH_MAX];
 
 void signal_handler(int signum) {
-    char msg[] = "Signal %d received by signal_handler()\n";
-    syslog(LOG_INFO, msg, signum);
-    printf(msg, signum);
     done = 1;
+    char msg[] = "Signal [  ] caught\n";
+    msg[8] = '0' + signum / 10;
+    msg[9] = '0' + signum % 10;
+    write(STDERR_FILENO, msg, strlen(msg));
 }
 
 void* thread_monitor_main_entrance() {
@@ -65,16 +66,16 @@ void* thread_monitor_main_entrance() {
                 CURL *curl;
                 CURLcode res;
                 curl = curl_easy_init();
-                if(curl) {          
+                if(curl) {
                     snprintf(url, PATH_MAX, telemetry_endpoint, 0);
-                    curl_easy_setopt(curl, CURLOPT_URL, url);  
+                    curl_easy_setopt(curl, CURLOPT_URL, url);
                     /* Perform the request, res will get the return code */
                     res = curl_easy_perform(curl);
                     /* Check for errors */
                     if(res != CURLE_OK)
                         syslog(LOG_ERR, "curl_easy_perform() failed: %s\n",
                             curl_easy_strerror(res));
-                
+
                     /* always cleanup */
                     curl_easy_cleanup(curl);
                 } else {
@@ -98,7 +99,7 @@ void* thread_monitor_rack_door() {
     const int pin_pos = 19;
     const int pin_neg = 16;
 
-    const char* sql_create = 
+    const char* sql_create =
         "CREATE TABLE IF NOT EXISTS door_state"
         "("
         "  [record_id] INTEGER PRIMARY KEY AUTOINCREMENT,"
@@ -117,8 +118,10 @@ void* thread_monitor_rack_door() {
     gpioWrite(pin_pos, PI_HIGH);
     gpioSetPullUpDown(pin_neg, PI_PUD_UP);
 
-    bool last_status = false;
-    bool current_status = false;
+    // 0 -> circuit opened -> door opened
+    // 1 -> circuit closed -> door closed
+    int last_status = 1;
+    int current_status = 1;
     size_t status_count = 0;
     while (!done) {
         usleep(1000 * 1000); // i.e., 1sec
@@ -128,21 +131,20 @@ void* thread_monitor_rack_door() {
             if (status_count < 5) {
                 continue;
             }
-            // true -> circuit closed -> door closed
             last_status = current_status;
 
-            int rc = sqlite3_open(db_path, &db);      
-            if (rc != SQLITE_OK) {         
+            int rc = sqlite3_open(db_path, &db);
+            if (rc != SQLITE_OK) {
                 syslog(LOG_ERR, "Cannot open database [%s]: %s. INSERT will be "
                 "skipped\n", db_path, sqlite3_errmsg(db));
                 sqlite3_close(db);
                 continue;
             }
-            rc = sqlite3_exec(db, sql_create, 0, 0, &sqlite_err_msg);      
-            if (rc != SQLITE_OK) {         
+            rc = sqlite3_exec(db, sql_create, 0, 0, &sqlite_err_msg);
+            if (rc != SQLITE_OK) {
                 syslog(LOG_ERR, "SQL error: %s. CREATE is not successful.\n",
-                    sqlite_err_msg);         
-                sqlite3_free(sqlite_err_msg);        
+                    sqlite_err_msg);
+                sqlite3_free(sqlite_err_msg);
                 sqlite3_close(db);
                 continue;
             }
@@ -151,13 +153,14 @@ void* thread_monitor_rack_door() {
             if(stmt != NULL) {
                 time(&now);
                 char buf[sizeof("1970-01-01 00:00:00")];
-                strftime(buf, sizeof buf, "%Y-%m-%d %H:%M:%S", localtime(&now)); 
+                strftime(buf, sizeof buf, "%Y-%m-%d %H:%M:%S", localtime(&now));
                 sqlite3_bind_text(stmt, 1, buf, -1, NULL);
                 sqlite3_bind_int(stmt, 2, !current_status);
-                // here we try to be consistent with common sense -> 1 means "triggered" and thus "opened"
+                /* here we try to be consistent with common sense:
+                  1 means "triggered" and thus "opened"*/
                 sqlite3_step(stmt);
                 rc = sqlite3_finalize(stmt);
-                if (rc != SQLITE_OK) {         
+                if (rc != SQLITE_OK) {
                     syslog(LOG_ERR,
                         "SQL error: %d. INSERT is not successful.\n", rc);
                     sqlite3_close(db);
@@ -177,7 +180,7 @@ void* thread_monitor_rack_door() {
 }
 
 void* thread_apply_fans_load(void* payload) {
-    
+
     syslog(LOG_INFO, "thread_apply_fans_load() started.");
     struct Payload* pl = (struct Payload*)payload;
     const char* sql_create = "CREATE TABLE IF NOT EXISTS temp_control"
@@ -200,7 +203,7 @@ void* thread_apply_fans_load(void* payload) {
     const uint16_t fans_pin = 23;
     gpioSetMode(fans_pin, PI_OUTPUT);
     gpioSetPWMfrequency(fans_pin, 50); // Set GPIO23 to 50Hz.
-    
+
     size_t interval = 3600;
     size_t iter = interval - 60;
     // we wait for 10 sec before first DB writing--
@@ -219,24 +222,24 @@ void* thread_apply_fans_load(void* payload) {
         if (gpioPWM(fans_pin, pl->fans_load * 254) != 0) {
             syslog(LOG_ERR, "Failed to set new fans load.");
             continue;
-        }      
+        }
         if (iter < interval) {
             continue;
         }
         iter = 0;
-        
-        
-        int rc = sqlite3_open(db_path, &db);      
-        if (rc != SQLITE_OK) {         
+
+
+        int rc = sqlite3_open(db_path, &db);
+        if (rc != SQLITE_OK) {
             syslog(LOG_ERR, "Cannot open database [%s]: %s. INSERT will be "
                 "skipped\n", db_path, sqlite3_errmsg(db));
             sqlite3_close(db);
             continue;
         }
-        rc = sqlite3_exec(db, sql_create, 0, 0, &sqlite_err_msg);      
-        if (rc != SQLITE_OK) {         
-            syslog(LOG_ERR, "SQL error: %s. CREATE is not successful.\n", sqlite_err_msg);         
-            sqlite3_free(sqlite_err_msg);        
+        rc = sqlite3_exec(db, sql_create, 0, 0, &sqlite_err_msg);
+        if (rc != SQLITE_OK) {
+            syslog(LOG_ERR, "SQL error: %s. CREATE is not successful.\n", sqlite_err_msg);
+            sqlite3_free(sqlite_err_msg);
             sqlite3_close(db);
             continue;
         }
@@ -245,7 +248,7 @@ void* thread_apply_fans_load(void* payload) {
         if(stmt != NULL) {
             time(&now);
             char buf[sizeof("1970-01-01 00:00:00")];
-            strftime(buf, sizeof buf, "%Y-%m-%d %H:%M:%S", localtime(&now)); 
+            strftime(buf, sizeof buf, "%Y-%m-%d %H:%M:%S", localtime(&now));
             sqlite3_bind_text(stmt, 1, buf, -1, NULL);
             sqlite3_bind_int(stmt, 2, pl->temps[0]);
             sqlite3_bind_int(stmt, 3, pl->temps[1]);
@@ -254,7 +257,7 @@ void* thread_apply_fans_load(void* payload) {
             sqlite3_bind_double(stmt, 6, pl->fans_load);
             sqlite3_step(stmt);
             rc = sqlite3_finalize(stmt);
-            if (rc != SQLITE_OK) {         
+            if (rc != SQLITE_OK) {
                 syslog(LOG_ERR, "SQL error: %d. INSERT is not successful.\n", rc);
                 sqlite3_close(db);
                 continue;
@@ -271,7 +274,7 @@ void* thread_apply_fans_load(void* payload) {
 void* thread_get_readings_from_sensors(void* payload) {
     syslog(LOG_INFO, "thread_get_readings_from_sensors() started.");
     struct Payload* pl = (struct Payload*)payload;
-    
+
     char sensors[4][256] = {
         "/sys/bus/w1/devices/28-0301a279faf2/w1_slave",
         "/sys/bus/w1/devices/28-030997792b61/w1_slave",
@@ -286,7 +289,7 @@ void* thread_get_readings_from_sensors(void* payload) {
             // takes around 1 sec to read value from one sensor.
             fd = open(sensors[i], O_RDONLY);
             if(fd >= 0) {
-                if(read( fd, buf, sizeof(buf) ) > 0) {          
+                if(read( fd, buf, sizeof(buf) ) > 0) {
                     char* temp_str = strstr(buf, "t=") + 2;
                     sscanf(temp_str, "%d", &(pl->temps[i]));
                 }
@@ -335,7 +338,7 @@ void* thread_set_7seg_display(void* payload) {
     return NULL;
 }
 
-int main() {
+int main(void) {
     openlog("rd.out", LOG_PID | LOG_CONS, 0);
     syslog(LOG_INFO, "rd.out started\n");
     pthread_t tids[5];
@@ -344,6 +347,19 @@ int main() {
         syslog(LOG_ERR, "pigpio initialisation failed, program will quit\n");
         closelog();
         return 1;
+    }
+    struct sigaction act;
+    act.sa_handler = signal_handler;
+    act.sa_flags = 0;
+    if (sigemptyset(&act.sa_mask) == -1) {
+        perror("sigemptyset()");
+        return EXIT_FAILURE;
+    }
+    if (sigaction(SIGINT,  &act, 0) == -1 ||
+        sigaction(SIGABRT, &act, 0) == -1 ||
+        sigaction(SIGTERM, &act, 0) == -1) {
+        perror("sigaction()");
+        return EXIT_FAILURE;
     }
     snprintf(db_path, PATH_MAX, "%s", getenv("RD_DB_DIR"));
     struct Payload pl;
@@ -357,7 +373,7 @@ int main() {
         pthread_create(&tids[2], NULL, thread_monitor_rack_door, NULL) != 0 ||
         pthread_create(&tids[3], NULL, thread_set_7seg_display, &pl) != 0 ||
         pthread_create(&tids[4], NULL, thread_monitor_main_entrance, NULL) != 0
-        
+
     ) {
         syslog(LOG_ERR, "Failed to create essential threads, program will quit\n");
         done = 1;
@@ -365,20 +381,12 @@ int main() {
         gpioTerminate();
         return 1;
     }
-
-    struct sigaction act;
-    act.sa_handler = signal_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_RESETHAND;
-    sigaction(SIGINT, &act, 0);
-    sigaction(SIGABRT, &act, 0);
-    sigaction(SIGTERM, &act, 0);
     for (size_t i = 0; i < sizeof(tids) / sizeof(tids[0]); ++i) {
         pthread_join(tids[i], NULL);
     }
     /* Stop DMA, release resources */
     gpioTerminate();
-    char exit_msg[] = "main() quits gracefully.\n";
+    const char exit_msg[] = "main() quits gracefully.\n";
     syslog(LOG_INFO, exit_msg);
     printf(exit_msg);
     closelog();
